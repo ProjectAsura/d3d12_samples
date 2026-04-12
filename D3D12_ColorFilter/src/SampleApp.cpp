4fbe72132e84b1018c2cca73b93d761a40922924
Pocol <project.asura@gmail>
2026-04-12 22:21:21 +0900

    カラーフィルタのサンプルを追加.

---
+++ D3D12_ColorFilter/src/SampleApp.cpp
@@ -0,0 +1,400 @@
+﻿//-----------------------------------------------------------------------------
+// File : SampleApp.cpp
+// Desc : Sample Application.
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <SampleApp.h>
+#include <fnd/asdxPath.h>
+#include <fnd/asdxLogger.h>
+#include <fnd/asdxFileIO.h>
+#include <fnd/asdxMisc.h>
+#include <edit/asdxGuiMgr.h>
+#include <gfx/asdxFade.h>
+
+#include "../external/asdx12/external/imgui/imgui.h"
+
+
+namespace {
+
+
+} // namespace
+
+///////////////////////////////////////////////////////////////////////////////
+// SampleApp class
+///////////////////////////////////////////////////////////////////////////////
+
+//-----------------------------------------------------------------------------
+//      コンストラクタです.
+//-----------------------------------------------------------------------------
+SampleApp::SampleApp()
+: asdx::App(L"Sample", 1920, 1080, nullptr, nullptr, nullptr)
+{
+    m_SwapChainFormat    = DXGI_FORMAT_R8G8B8A8_UNORM;
+    m_DepthStencilFormat = DXGI_FORMAT_D32_FLOAT;
+
+    m_DeviceDesc.MaxShaderResourceCount = 8192;
+    m_DeviceDesc.MaxSamplerCount        = 128;
+    m_DeviceDesc.MaxColorTargetCount    = 256;
+    m_DeviceDesc.MaxDepthTargetCount    = 256;
+
+#if ASDX_DEBUG
+    m_DeviceDesc.EnableDebug   = true;
+    m_DeviceDesc.EnableCapture = true;
+#endif
+}
+
+//-----------------------------------------------------------------------------
+//      デストラクタです.
+//-----------------------------------------------------------------------------
+SampleApp::~SampleApp()
+{
+}
+
+//-----------------------------------------------------------------------------
+//      初期化処理です.
+//-----------------------------------------------------------------------------
+bool SampleApp::OnInit()
+{
+    auto pDevice = asdx::GetD3D12Device();
+
+    #if ASDX_ENABLE_SOUND
+    {
+        // サウンドマネージャの初期化.
+        asdx::InitSoundMgr(reinterpret_cast<uintptr_t>(m_hWnd));
+    }
+    #endif
+
+    // コマンドリストをリセット.
+    m_GfxCmdList.Reset();
+    auto pCmd = m_GfxCmdList.GetD3D12CommandList();
+
+    #if ASDX_ENABLE_IMGUI
+    // GUI初期化.
+    {
+        if (!asdx::GuiMgr::Instance().Init(pCmd, m_hWnd, m_Width, m_Height, m_SwapChainFormat))
+        {
+            ELOGA("Error : GuiMgr::Init() Failed.");
+            return false;
+        }
+    }
+    #endif
+
+    // テクスチャマネージャ初期化.
+    if (!asdx::TextureManager::Instance().Init())
+    {
+        ELOGA("Error : TextureManager::Init() Failed.");
+        return false;
+    }
+
+    // テクスチャ初期化.
+    {
+        asdx::fs::path input = "../res/textures/Test0.txb";
+        asdx::fs::path path;
+        if (!asdx::SearchFilePath(input, path))
+        {
+            ELOGA("Error : File Not Found. path= %s", input.string().c_str());
+            return false;
+        }
+
+        m_TextureBG = asdx::TextureManager::Instance().GetOrCreate(path.string().c_str());
+        if (!m_TextureBG.IsValid())
+        {
+            ELOGA("Error : Texture Load Failed.");
+            return false;
+        }
+    }
+
+    // サンプラー初期化.
+    if (!m_LinearClamp.Init(&asdx::Sampler::LinearClamp))
+    {
+        ELOGA("Error : Sampler::Init() Failed.");
+        return false;
+    }
+
+    // カラーフィルタ初期化.
+    if (!m_ColorFilter.Init(m_SwapChainFormat))
+    {
+        ELOGA("Error : ColorFilter::Init() Failed.");
+        return false;
+    }
+
+    // コマンドの記録を終了.
+    pCmd->Close();
+
+    // セットアップコマンド実行.
+    {
+        ID3D12CommandList* pCmds[] = { pCmd };
+
+        // グラフィックスキューを取得.
+        auto pGraphicsQueue = asdx::GetGraphicsQueue();
+
+        // コマンドを実行.
+        pGraphicsQueue->Execute(_countof(pCmds), pCmds);
+
+        // 待機点を発行.
+        m_FrameWaitPoint = pGraphicsQueue->Signal();
+
+        // 完了を待機.
+        pGraphicsQueue->Sync(m_FrameWaitPoint);
+    }
+
+    return true;
+}
+
+//-----------------------------------------------------------------------------
+//      終了時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnTerm()
+{
+    // テクスチャ解放.
+    m_TextureBG.Reset();
+
+    // テクスチャマネージャ終了処理.
+    asdx::TextureManager::Instance().Term();
+
+    // サンプラー解放.
+    m_LinearClamp.Term();
+
+    // カラーフィルタ解放.
+    m_ColorFilter.Term();
+
+    #if ASDX_ENABLE_IMGUI
+    {
+        // GUI終了処理.
+        asdx::GuiMgr::Instance().Term();
+    }
+    #endif
+
+    #if ASDX_ENABLE_SOUND
+    {
+        // サウンドマネージャの終了処理.
+        asdx::TermSoundMgr();
+    }
+    #endif
+}
+
+//-----------------------------------------------------------------------------
+//      フレーム遷移時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnFrameMove(const asdx::App::FrameEventArgs& args)
+{
+    #if ASDX_ENABLE_IMGUI
+    {
+        // ImGuiフレーム開始処理.
+        asdx::GuiMgr::Instance().Update(m_Width, m_Height);
+
+        ImGui::SetNextWindowSize(ImVec2(300, 230), ImGuiCond_Once);
+        if (ImGui::Begin(ASDX_U8("カラーフィルタ")))
+        {
+            auto saturation = m_ColorFilter.GetSaturation();
+            if (ImGui::DragFloat3(ASDX_U8("彩度"), &saturation.x, 0.1f))
+            { m_ColorFilter.SetSaturation(saturation); }
+
+            auto brightness = m_ColorFilter.GetBrightness();
+            if (ImGui::DragFloat(ASDX_U8("明度"), &brightness, 0.01f))
+            { m_ColorFilter.SetBrightness(brightness); }
+
+            auto contrast = m_ColorFilter.GetContrast();
+            if (ImGui::DragFloat(ASDX_U8("コントラスト"), &contrast, 0.01f))
+            { m_ColorFilter.SetContrast(contrast); }
+
+            auto hue = m_ColorFilter.GetHueDegree();
+            if (ImGui::DragFloat(ASDX_U8("色相"), &hue, 0.1f, 0.0f, 360.0f))
+            { m_ColorFilter.SetHueDegree(hue); }
+
+            auto sepiaTone = m_ColorFilter.GetSepiaTone();
+            if (ImGui::DragFloat(ASDX_U8("セピアトーン"), &sepiaTone, 0.01f, 0.0f, 1.0f))
+            { m_ColorFilter.SetSepiaTone(sepiaTone); }
+
+            auto grayScale = m_ColorFilter.GetGrayScale();
+            if (ImGui::DragFloat(ASDX_U8("グレースケール"), &grayScale, 0.01f, 0.0f, 1.0f))
+            { m_ColorFilter.SetGrayScale(grayScale); }
+
+            auto reverse = m_ColorFilter.IsReverse();
+            if (ImGui::Checkbox(ASDX_U8("色反転"), &reverse))
+            { m_ColorFilter.SetReverse(reverse); }
+
+            if (ImGui::Button(ASDX_U8("リセット")))
+            { m_ColorFilter.ResetValues(); }
+
+            ImGui::End();
+        }
+    }
+    #endif
+}
+
+//-----------------------------------------------------------------------------
+//      フレーム描画時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnFrameRender(const asdx::App::FrameEventArgs& args)
+{
+    auto idx = GetCurrentBackBufferIndex();
+
+    // コマンド記録開始.
+    m_GfxCmdList.Reset();
+    auto pCmd = m_GfxCmdList.GetD3D12CommandList();
+
+    {
+        D3D12_RESOURCE_BARRIER barrier = {};
+        barrier.Type                    = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+        barrier.Transition.pResource    = m_ColorTarget[idx].GetResource();
+        barrier.Transition.StateBefore  = D3D12_RESOURCE_STATE_PRESENT;
+        barrier.Transition.StateAfter   = D3D12_RESOURCE_STATE_RENDER_TARGET;
+        pCmd->ResourceBarrier(1, &barrier);
+    }
+
+    auto handleRTV = m_ColorTarget[idx].GetCpuHandleRTV();
+    auto handleDSV = m_DepthTarget.GetCpuHandleDSV();
+    pCmd->OMSetRenderTargets(1, &handleRTV, FALSE, &handleDSV);
+    pCmd->ClearRenderTargetView(handleRTV, m_ClearColor, 0, nullptr);
+    pCmd->ClearDepthStencilView(handleDSV, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
+    pCmd->RSSetViewports(1, &m_Viewport);
+    pCmd->RSSetScissorRects(1, &m_ScissorRect);
+
+ 
+    // カラーフィルタ描画.
+    m_ColorFilter.Draw(pCmd, m_TextureBG.GetHandleGPU());
+
+    #if ASDX_ENABLE_IMGUI
+    {
+        // ImGui描画処理.
+        asdx::GuiMgr::Instance().Draw(pCmd);
+    }
+    #endif
+
+    {
+        D3D12_RESOURCE_BARRIER barrier = {};
+        barrier.Type                    = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+        barrier.Transition.pResource    = m_ColorTarget[idx].GetResource();
+        barrier.Transition.StateBefore  = D3D12_RESOURCE_STATE_RENDER_TARGET;
+        barrier.Transition.StateAfter   = D3D12_RESOURCE_STATE_PRESENT;
+        pCmd->ResourceBarrier(1, &barrier);
+    }
+
+    // コマンド記録終了.
+    pCmd->Close();
+
+    // コマンドを実行.
+    {
+        ID3D12CommandList* pCmds[] = { pCmd };
+
+        auto pGraphicsQueue = asdx::GetGraphicsQueue();
+
+        // 前フレームの描画の完了を待機.
+        if (m_FrameWaitPoint.IsValid())
+        { pGraphicsQueue->Sync(m_FrameWaitPoint); }
+
+        // コマンドを実行.
+        pGraphicsQueue->Execute(_countof(pCmds), pCmds);
+
+        // 待機点を発行.
+        m_FrameWaitPoint = pGraphicsQueue->Signal();
+    }
+
+    // 画面に表示.
+    Present(1);
+
+    // フレーム同期.
+    asdx::FrameSync();
+}
+
+//-----------------------------------------------------------------------------
+//      リサイズ時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnResize(const asdx::App::ResizeEventArgs& args)
+{
+    // TODO : Implementation.
+    {
+    }
+}
+
+//-----------------------------------------------------------------------------
+//      キー処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnKey(const asdx::App::KeyEventArgs& args)
+{
+    m_Camera.OnKey(args.KeyCode, args.IsKeyDown, args.IsAltDown);
+    #if ASDX_ENABLE_IMGUI
+    {
+        // ImGuiのキー処理.
+        asdx::GuiMgr::Instance().OnKey(args.KeyCode, args.IsKeyDown, args.IsAltDown);
+    }
+    #endif
+}
+
+//-----------------------------------------------------------------------------
+//      マウス処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnMouse(const asdx::App::MouseEventArgs& args)
+{
+    if(args.IsAltDown)
+    {
+        // カメラのマウス処理.
+        m_Camera.OnMouse(
+            args.X,
+            args.Y,
+            args.WheelDelta,
+            args.IsDownL,
+            args.IsDownR,
+            args.IsDownM,
+            args.IsDownX1,
+            args.IsDownX2);
+    }
+    else
+    {
+        #if ASDX_ENABLE_IMGUI
+        {
+            // ImGuiのマウス処理.
+            asdx::GuiMgr::Instance().OnMouse(
+                args.X,
+                args.Y,
+                args.WheelDelta,
+                args.IsDownL,
+                args.IsDownM,
+                args.IsDownR);
+        }
+        #endif
+    }
+}
+
+//-----------------------------------------------------------------------------
+//      タイピング処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnTyping(uint32_t keyCode)
+{
+    #if ASDX_ENABLE_IMGUI
+    {
+        // タイピング時の処理です.
+        asdx::GuiMgr::Instance().OnTyping(keyCode);
+    }
+    #endif
+}
+
+//-----------------------------------------------------------------------------
+//      メッセージプロシージャ時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnMsgProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    ASDX_UNUSED(hWnd);
+    ASDX_UNUSED(wp);
+    ASDX_UNUSED(lp);
+
+    switch(msg)
+    {
+    case MM_MCINOTIFY:
+        {
+        #if ASDX_ENABLE_SOUND
+            // サウンドマネージャのコールバック.
+            asdx::OnSoundMsg(uint32_t(lp), uint32_t(wp));
+        #endif//ASDX_ENABLE_SOUND
+        }
+        break;
+
+    default:
+        break;
+    }
+}

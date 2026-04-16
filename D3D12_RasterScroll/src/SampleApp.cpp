d0cafcc6cb34a030ded6258fe08a4eb55351f542
Pocol <project.asura@gmail>
2026-04-16 23:54:33 +0900

    ラスタースクロールのサンプルを追加.

---
+++ D3D12_RasterScroll/src/SampleApp.cpp
@@ -0,0 +1,456 @@
+﻿//-----------------------------------------------------------------------------
+// File : SampleApp.cpp
+// Desc : Sample Application.
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <SampleApp.h>
+#include <fnd/asdxPath.h>
+#include <fnd/asdxLogger.h>
+#include <fnd/asdxFileIO.h>
+#include <edit/asdxGuiMgr.h>
+#include <gfx/asdxPresetState.h>
+
+
+namespace {
+
+//-----------------------------------------------------------------------------
+// Shaders
+//-----------------------------------------------------------------------------
+#include "../res/shaders/Compiled/RasterScrollPS.inc"
+
+///////////////////////////////////////////////////////////////////////////////
+// Param structure
+///////////////////////////////////////////////////////////////////////////////
+struct Param
+{
+    asdx::Vector2 Offset;
+    asdx::Vector2 Scale;
+};
+
+} // namespace
+
+///////////////////////////////////////////////////////////////////////////////
+// SampleApp class
+///////////////////////////////////////////////////////////////////////////////
+
+//-----------------------------------------------------------------------------
+//      コンストラクタです.
+//-----------------------------------------------------------------------------
+SampleApp::SampleApp()
+: asdx::App(L"Sample", 1920, 1080, nullptr, nullptr, nullptr)
+{
+    m_SwapChainFormat    = DXGI_FORMAT_R8G8B8A8_UNORM;
+    m_DepthStencilFormat = DXGI_FORMAT_D32_FLOAT;
+
+    m_DeviceDesc.MaxShaderResourceCount = 8192;
+    m_DeviceDesc.MaxSamplerCount        = 128;
+    m_DeviceDesc.MaxColorTargetCount    = 256;
+    m_DeviceDesc.MaxDepthTargetCount    = 256;
+
+#if ASDX_DEBUG
+    m_DeviceDesc.EnableDebug   = true;
+    m_DeviceDesc.EnableCapture = true;
+#endif
+}
+
+//-----------------------------------------------------------------------------
+//      デストラクタです.
+//-----------------------------------------------------------------------------
+SampleApp::~SampleApp()
+{
+}
+
+//-----------------------------------------------------------------------------
+//      初期化処理です.
+//-----------------------------------------------------------------------------
+bool SampleApp::OnInit()
+{
+    auto pDevice = asdx::GetD3D12Device();
+
+    #if ASDX_ENABLE_SOUND
+    {
+        // サウンドマネージャの初期化.
+        asdx::InitSoundMgr(reinterpret_cast<uintptr_t>(m_hWnd));
+    }
+    #endif
+
+    // コマンドリストをリセット.
+    m_GfxCmdList.Reset();
+    auto pCmd = m_GfxCmdList.GetD3D12CommandList();
+
+    #if ASDX_ENABLE_IMGUI
+    // GUI初期化.
+    {
+        if (!asdx::GuiMgr::Instance().Init(pCmd, m_hWnd, m_Width, m_Height, m_SwapChainFormat))
+        {
+            ELOGA("Error : GuiMgr::Init() Failed.");
+            return false;
+        }
+    }
+    #endif
+
+    // テクスチャマネージャ初期化.
+    if (!asdx::TextureManager::Instance().Init())
+    {
+        ELOGA("Error : TextureManager::Init() Failed.");
+        return false;
+    }
+
+    // テクスチャ生成.
+    {
+        asdx::fs::path input = "../res/textures/Test0.txb";
+        asdx::fs::path path;
+        if (!asdx::SearchFilePath(input, path))
+        {
+            ELOGA("Error : File Not Found. path= %s", input.string().c_str());
+            return false;
+        }
+
+        m_TextureBG = asdx::TextureManager::Instance().GetOrCreate(path.string().c_str());
+        if (!m_TextureBG.IsValid())
+        {
+            ELOGA("Error : Texture Load Failed.");
+            return false;
+        }
+    }
+
+    // ルートシグニチャの初期化.
+    {
+        D3D12_DESCRIPTOR_RANGE range[1] = {};
+        range[0].RangeType                          = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+        range[0].NumDescriptors                     = 1;
+        range[0].BaseShaderRegister                 = 0;
+        range[0].RegisterSpace                      = 0;
+        range[0].OffsetInDescriptorsFromTableStart  = 0;
+
+        D3D12_ROOT_PARAMETER param[2] = {};
+        param[0].ParameterType              = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+        param[0].Constants.Num32BitValues   = 4;
+        param[0].Constants.ShaderRegister   = 0;
+        param[0].Constants.RegisterSpace    = 0;
+        param[0].ShaderVisibility           = D3D12_SHADER_VISIBILITY_PIXEL;
+
+        param[1].ParameterType                          = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+        param[1].DescriptorTable.NumDescriptorRanges    = 1;
+        param[1].DescriptorTable.pDescriptorRanges      = &range[0];
+        param[1].ShaderVisibility                       = D3D12_SHADER_VISIBILITY_PIXEL;
+
+        D3D12_ROOT_SIGNATURE_DESC desc = {};
+        desc.NumParameters      = _countof(param);
+        desc.pParameters        = param;
+        desc.NumStaticSamplers  = _countof(asdx::Preset::StaticSamplers);
+        desc.pStaticSamplers    = asdx::Preset::StaticSamplers;
+        desc.Flags              = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+        desc.Flags             |= D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS;
+        desc.Flags             |= D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS;
+        desc.Flags             |= D3D12_ROOT_SIGNATURE_FLAG_DENY_GEOMETRY_SHADER_ROOT_ACCESS;
+
+        asdx::RefPtr<ID3DBlob> blob;
+        asdx::RefPtr<ID3DBlob> errorBlob;
+        auto hr = D3D12SerializeRootSignature(&desc, D3D_ROOT_SIGNATURE_VERSION_1_0, blob.GetAddress(), errorBlob.GetAddress());
+        if (FAILED(hr))
+        {
+            ELOG("Error : D3D12SerializeRootSignature() Failed. errcode = 0x%x", hr);
+            if (errorBlob.GetPtr() != nullptr)
+            {
+                ELOG("Error : Msg = %s", reinterpret_cast<const char*>(errorBlob->GetBufferPointer()));
+            }
+            return false;
+        }
+
+        hr = pDevice->CreateRootSignature(0, blob->GetBufferPointer(), blob->GetBufferSize(), IID_PPV_ARGS(m_RootSignature.GetAddress()));
+        if (FAILED(hr))
+        {
+            ELOG("Error : ID3D12Device::CreateRootSignature() Failed. errcode = 0x%x", hr);
+            return false;
+        }
+    }
+
+    // グラフィックスパイプラインステートの初期化.
+    {
+        D3D12_GRAPHICS_PIPELINE_STATE_DESC desc = {};
+        desc.pRootSignature                 = m_RootSignature.GetPtr();
+        desc.VS                             = asdx::Preset::FullScreenVS;
+        desc.PS                             = { RasterScrollPS, sizeof(RasterScrollPS) };
+        desc.BlendState                     = asdx::Preset::Opaque;
+        desc.SampleMask                     = D3D12_DEFAULT_SAMPLE_MASK;
+        desc.RasterizerState                = asdx::Preset::CullNone;
+        desc.DepthStencilState              = asdx::Preset::DepthNone;
+        desc.InputLayout.NumElements        = _countof(asdx::Preset::QuadElements);
+        desc.InputLayout.pInputElementDescs = asdx::Preset::QuadElements;
+        desc.PrimitiveTopologyType          = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+        desc.NumRenderTargets               = 1;
+        desc.RTVFormats[0]                  = m_SwapChainFormat;
+        desc.DSVFormat                      = DXGI_FORMAT_UNKNOWN;
+        desc.SampleDesc.Count               = 1;
+        desc.SampleDesc.Quality             = 0;
+
+        auto hr = pDevice->CreateGraphicsPipelineState(&desc, IID_PPV_ARGS(m_PipelineState.GetAddress()));
+        if (FAILED(hr))
+        {
+            ELOGA("Error : ID3D12Device::CreateGraphicsPipelineState() Failed. errcode = 0x%x", hr);
+            return false;
+        }
+    }
+
+    // コマンドの記録を終了.
+    pCmd->Close();
+
+    // セットアップコマンド実行.
+    {
+        ID3D12CommandList* pCmds[] = { pCmd };
+
+        // グラフィックスキューを取得.
+        auto pGraphicsQueue = asdx::GetGraphicsQueue();
+
+        // コマンドを実行.
+        pGraphicsQueue->Execute(_countof(pCmds), pCmds);
+
+        // 待機点を発行.
+        m_FrameWaitPoint = pGraphicsQueue->Signal();
+
+        // 完了を待機.
+        pGraphicsQueue->Sync(m_FrameWaitPoint);
+    }
+
+    return true;
+}
+
+//-----------------------------------------------------------------------------
+//      終了時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnTerm()
+{
+    m_TextureBG.Reset();
+
+    asdx::TextureManager::Instance().Term();
+
+    m_PipelineState.Reset();
+    m_RootSignature.Reset();
+
+
+    #if ASDX_ENABLE_IMGUI
+    {
+        // GUI終了処理.
+        asdx::GuiMgr::Instance().Term();
+    }
+    #endif
+
+    #if ASDX_ENABLE_SOUND
+    {
+        // サウンドマネージャの終了処理.
+        asdx::TermSoundMgr();
+    }
+    #endif
+}
+
+//-----------------------------------------------------------------------------
+//      フレーム遷移時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnFrameMove(const asdx::App::FrameEventArgs& args)
+{
+    #if ASDX_ENABLE_IMGUI
+    {
+        // ImGuiフレーム開始処理.
+        asdx::GuiMgr::Instance().Update(m_Width, m_Height);
+    }
+    #endif
+
+    const auto kAmplitude = 50.0f;
+    const auto kSpeed     = 0.001f;
+
+    auto move = cosf(asdx::F_2PI * m_Counter * kSpeed);
+
+    m_ScrollOffset.x = 0.0f;
+    m_ScrollOffset.y += kSpeed * asdx::Sign(move);
+
+    m_ScrollScale.x = 1.0f;
+    m_ScrollScale.y = (0.5f - move * 0.5f) * kAmplitude;
+    m_Counter++;
+}
+
+//-----------------------------------------------------------------------------
+//      フレーム描画時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnFrameRender(const asdx::App::FrameEventArgs& args)
+{
+    auto idx = GetCurrentBackBufferIndex();
+
+    // コマンド記録開始.
+    m_GfxCmdList.Reset();
+    auto pCmd = m_GfxCmdList.GetD3D12CommandList();
+
+    {
+        D3D12_RESOURCE_BARRIER barrier = {};
+        barrier.Type                    = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+        barrier.Transition.pResource    = m_ColorTarget[idx].GetResource();
+        barrier.Transition.StateBefore  = D3D12_RESOURCE_STATE_PRESENT;
+        barrier.Transition.StateAfter   = D3D12_RESOURCE_STATE_RENDER_TARGET;
+        pCmd->ResourceBarrier(1, &barrier);
+    }
+
+    auto handleRTV = m_ColorTarget[idx].GetCpuHandleRTV();
+    auto handleDSV = m_DepthTarget.GetCpuHandleDSV();
+    pCmd->OMSetRenderTargets(1, &handleRTV, FALSE, &handleDSV);
+    pCmd->ClearRenderTargetView(handleRTV, m_ClearColor, 0, nullptr);
+    pCmd->ClearDepthStencilView(handleDSV, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
+    pCmd->RSSetViewports(1, &m_Viewport);
+    pCmd->RSSetScissorRects(1, &m_ScissorRect);
+
+    // ラスタースクロール描画.
+    {
+        Param param = {};
+        param.Offset = m_ScrollOffset;
+        param.Scale  = m_ScrollScale;
+
+        pCmd->SetGraphicsRootSignature(m_RootSignature.GetPtr());
+        pCmd->SetPipelineState(m_PipelineState.GetPtr());
+        pCmd->SetGraphicsRoot32BitConstants(0, 4, &param, 0);
+        pCmd->SetGraphicsRootDescriptorTable(1, m_TextureBG.GetHandleGPU());
+        asdx::DrawQuad(pCmd);
+    }
+
+
+    #if ASDX_ENABLE_IMGUI
+    {
+        // ImGui描画処理.
+        asdx::GuiMgr::Instance().Draw(pCmd);
+    }
+    #endif
+
+    {
+        D3D12_RESOURCE_BARRIER barrier = {};
+        barrier.Type                    = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+        barrier.Transition.pResource    = m_ColorTarget[idx].GetResource();
+        barrier.Transition.StateBefore  = D3D12_RESOURCE_STATE_RENDER_TARGET;
+        barrier.Transition.StateAfter   = D3D12_RESOURCE_STATE_PRESENT;
+        pCmd->ResourceBarrier(1, &barrier);
+    }
+
+    // コマンド記録終了.
+    pCmd->Close();
+
+    // コマンドを実行.
+    {
+        ID3D12CommandList* pCmds[] = { pCmd };
+
+        auto pGraphicsQueue = asdx::GetGraphicsQueue();
+
+        // 前フレームの描画の完了を待機.
+        if (m_FrameWaitPoint.IsValid())
+        { pGraphicsQueue->Sync(m_FrameWaitPoint); }
+
+        // コマンドを実行.
+        pGraphicsQueue->Execute(_countof(pCmds), pCmds);
+
+        // 待機点を発行.
+        m_FrameWaitPoint = pGraphicsQueue->Signal();
+    }
+
+    // 画面に表示.
+    Present(1);
+
+    // フレーム同期.
+    asdx::FrameSync();
+}
+
+//-----------------------------------------------------------------------------
+//      リサイズ時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnResize(const asdx::App::ResizeEventArgs& args)
+{
+    // TODO : Implementation.
+    {
+    }
+}
+
+//-----------------------------------------------------------------------------
+//      キー処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnKey(const asdx::App::KeyEventArgs& args)
+{
+    m_Camera.OnKey(args.KeyCode, args.IsKeyDown, args.IsAltDown);
+    #if ASDX_ENABLE_IMGUI
+    {
+        // ImGuiのキー処理.
+        asdx::GuiMgr::Instance().OnKey(args.KeyCode, args.IsKeyDown, args.IsAltDown);
+    }
+    #endif
+}
+
+//-----------------------------------------------------------------------------
+//      マウス処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnMouse(const asdx::App::MouseEventArgs& args)
+{
+    if(args.IsAltDown)
+    {
+        // カメラのマウス処理.
+        m_Camera.OnMouse(
+            args.X,
+            args.Y,
+            args.WheelDelta,
+            args.IsDownL,
+            args.IsDownR,
+            args.IsDownM,
+            args.IsDownX1,
+            args.IsDownX2);
+    }
+    else
+    {
+        #if ASDX_ENABLE_IMGUI
+        {
+            // ImGuiのマウス処理.
+            asdx::GuiMgr::Instance().OnMouse(
+                args.X,
+                args.Y,
+                args.WheelDelta,
+                args.IsDownL,
+                args.IsDownM,
+                args.IsDownR);
+        }
+        #endif
+    }
+}
+
+//-----------------------------------------------------------------------------
+//      タイピング処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnTyping(uint32_t keyCode)
+{
+    #if ASDX_ENABLE_IMGUI
+    {
+        // タイピング時の処理です.
+        asdx::GuiMgr::Instance().OnTyping(keyCode);
+    }
+    #endif
+}
+
+//-----------------------------------------------------------------------------
+//      メッセージプロシージャ時の処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnMsgProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+    ASDX_UNUSED(hWnd);
+    ASDX_UNUSED(wp);
+    ASDX_UNUSED(lp);
+
+    switch(msg)
+    {
+    case MM_MCINOTIFY:
+        {
+        #if ASDX_ENABLE_SOUND
+            // サウンドマネージャのコールバック.
+            asdx::OnSoundMsg(uint32_t(lp), uint32_t(wp));
+        #endif//ASDX_ENABLE_SOUND
+        }
+        break;
+
+    default:
+        break;
+    }
+}
